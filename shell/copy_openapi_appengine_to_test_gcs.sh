fd0262102ae924c18343e854625cd56cd0219027
BillClifford <bcli4d>
2025-07-09 13:36:51 -0700

    Remove support for user cohorts.

--- shell/copy_openapi_appengine_to_test_gcs.sh
+++ shell/copy_openapi_appengine_to_test_gcs.sh
@@ -6,14 +6,14 @@ DATE=$(date '+%Y%m%d_%H%M%S')
 
 cp openapi-appengine.v1.yaml test.openapi-appengine.v1.yaml
 sed -i "" 's/#host: "testing-api.canceridc.dev"/host: "testing-api.canceridc.dev"/' test.openapi-appengine.v1.yaml
-sed -i "" 's/x-google-audiences: "94/#x-google-audiences: "94/'  test.openapi-appengine.v1.yaml
-sed -i "" 's/#x-google-audiences: "10/x-google-audiences: "10/'  test.openapi-appengine.v1.yaml
+#sed -i "" 's/x-google-audiences: "94/#x-google-audiences: "94/'  test.openapi-appengine.v1.yaml
+#sed -i "" 's/#x-google-audiences: "10/x-google-audiences: "10/'  test.openapi-appengine.v1.yaml
 sed -i "" 's/#- "https"/- "https"/' test.openapi-appengine.v1.yaml
 sed -i "" 's/- "http"/#- "http"/' test.openapi-appengine.v1.yaml
 cp openapi-appengine.v2.yaml test.openapi-appengine.v2.yaml
 sed -i "" 's/#host: "testing-api.canceridc.dev"/host: "testing-api.canceridc.dev"/' test.openapi-appengine.v2.yaml
-sed -i "" 's/x-google-audiences: "94/#x-google-audiences: "94/'  test.openapi-appengine.v2.yaml
-sed -i "" 's/#x-google-audiences: "10/x-google-audiences: "10/'  test.openapi-appengine.v2.yaml
+#sed -i "" 's/x-google-audiences: "94/#x-google-audiences: "94/'  test.openapi-appengine.v2.yaml
+#sed -i "" 's/#x-google-audiences: "10/x-google-audiences: "10/'  test.openapi-appengine.v2.yaml
 sed -i "" 's/#- "https"/- "https"/' test.openapi-appengine.v2.yaml
 sed -i "" 's/- "http"/#- "http"/' test.openapi-appengine.v2.yaml
 
